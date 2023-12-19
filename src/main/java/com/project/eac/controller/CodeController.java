421f2a5c91094692bc2862db6effb05baf47573e
DreamingLri <3507415455@qq.com>
2023-12-19 17:59:18 +0800

    add a test

--- src/main/java/com/project/eac/controller/CodeController.java
+++ src/main/java/com/project/eac/controller/CodeController.java
@@ -4,6 +4,7 @@ import com.project.eac.entity.Codes;
 import com.project.eac.service.CodesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,9 @@ public class CodeController {
     @GetMapping("/getAllCodes")
     public List<Codes> getAllCodes(){
         return codesService.getAllCodes();
+    }
+    @PostMapping("/test")
+    public String test(){
+        return "hello";
     }
 }
