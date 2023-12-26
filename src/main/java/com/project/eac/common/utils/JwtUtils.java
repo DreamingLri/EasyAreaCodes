9ed6f3513f09acfa9376e7c7750a2d97f1587101
DreamingLri <3507415455@qq.com>
2023-12-26 12:29:31 +0800

    add jwt

---
+++ src/main/java/com/project/eac/common/utils/JwtUtils.java
@@ -0,0 +1,57 @@
+package com.project.eac.common.utils;
+
+import com.project.eac.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtUtils {
+    private String secret;
+    private String tokenHeader;
+    private Integer expiration;
+
+    private Date generateExpirationDate(){
+        return new Date(System.currentTimeMillis()+expiration*1000);
+    }
+
+    public String generateToken(Map<String, Object> claim){
+        return Jwts.builder()
+                .setClaims(claim)
+                .setExpiration(generateExpirationDate())
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
+
+    public String generateToken(User user){
+        Map<String, Object> map = new HashMap<>();
+        String CLAIM_KEY_CREATED = "created";
+        map.put(CLAIM_KEY_CREATED, user.getUserName());
+        String CLAIM_KEY_USERNAME = "sub";
+        map.put(CLAIM_KEY_USERNAME, new Date());
+        return generateToken(map);
+    }
+
+    public Claims getClaims(String token){
+        Claims claims = null;
+
+        try {
+            claims = Jwts.parser()
+                    .setSigningKey(secret)
+                    .parseClaimsJws(token)
+                    .getBody();
+        }catch (ExpiredJwtException e){
+            claims = e.getClaims();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return claims;
+    }
+}
